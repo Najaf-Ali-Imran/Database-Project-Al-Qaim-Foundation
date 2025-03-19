c3125ee0003c8efcc8634ecbf563027d68c31e15
Amna1005 <amna.zahra.2405@gmail.com>
2025-03-19 13:38:56 +0500

    sample collection

---
+++ frontend/SampleCollectionPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace AIQaimFoundation
+{
+    /// <summary>
+    /// Interaction logic for SampleCollectionPage.xaml
+    /// </summary>
+    public partial class SampleCollectionPage : Window
+    {
+        public SampleCollectionPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class SampleData
+    {
+        public required string Id { get; set; }
+        public required string PatientName { get; set; }
+        public required string SampleType { get; set; }
+        public required string CollectionDate { get; set; }
+        public required string Status { get; set; }
+    }
+}
